528c00cc1626de8a0b00bd48f0d786e87429ff52
Mulia Andiki <muliaandiki@gmail.com>
2025-12-12 18:42:48 +0700

    feat: intergrate fast api to elysia js

---
+++ be/src/utils/axios.ts
@@ -0,0 +1,14 @@
+import axios, { AxiosInstance, AxiosError } from "axios";
+import { env } from "@/config/env.config";
+
+export function AxiosService() {
+  const AxiosHit: AxiosInstance = axios.create({
+    baseURL: env.SERVICE_APP,
+    timeout: 50000,
+  });
+
+  return {
+    AxiosHit,
+    isAxiosError: axios.isAxiosError,
+  };
+}
---
+++ be/src/utils/cleanValues.ts
@@ -0,0 +1,30 @@
+export function cleanNaNValues(obj: any): any {
+  if (obj === null || obj === undefined) {
+    return null;
+  }
+
+  if (Array.isArray(obj)) {
+    return obj.map((item) => cleanNaNValues(item));
+  }
+
+  if (typeof obj === "object") {
+    const cleaned: any = {};
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        const value = obj[key];
+
+        // Ganti NaN, undefined string "NaN", "Nan"
+        if (typeof value === "number" && isNaN(value)) {
+          cleaned[key] = null;
+        } else if (value === "NaN" || value === "Nan") {
+          cleaned[key] = null;
+        } else {
+          cleaned[key] = cleanNaNValues(value);
+        }
+      }
+    }
+    return cleaned;
+  }
+
+  return obj;
+}
